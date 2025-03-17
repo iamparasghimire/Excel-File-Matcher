cc77bce22154c94b8cfbdd8a810b3b7aa157e9f1
Paras Ghimire <72784083+iamparasghimire@users.noreply.github.com>
2025-03-17 13:54:37 +0545

    Delete .history/myproject/myapp directory

--- .history/myproject/myapp/forms_20250317130829.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class ExcelUploadForm(forms.Form):
-    file1 = forms.FileField(label="Upload First Excel File")
-    file2 = forms.FileField(label="Upload Second Excel File")
--- .history/myproject/myapp/urls_20250317131140.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import match_excel_view
-
-urlpatterns = [
-    path("", match_excel_view, name="match_excel"),
-]
